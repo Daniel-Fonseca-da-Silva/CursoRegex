34039ff203df9f6c9debcefef438e535134adbd0
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2020-03-12 15:10:34 -0300

    Adicionada ao projeto a pasta ExecutandoRegex com os arquivos em diversas linguagens de programação

---
+++ ExecutandoRegex/UsandoJava.java
@@ -0,0 +1,23 @@
+package ExecuntandoRegex;
+
+import java.util.regex.*;
+
+public class UsandoJava {
+    public static void main(final String[] args) {
+        
+        final String texto = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
+        
+        Pattern patternLetras = Pattern.compile("[a-f]");
+
+        Matcher matcher = patternLetras.matcher(texto);
+
+        while(matcher.find())
+        {
+            System.out.printf("Posicoes: %s, %s\tValor: %s%n",
+            matcher.start(), matcher.end(), matcher.group());
+        }
+
+        System.out.println(texto.replaceAll("[7-9]", "Mudou!"));
+
+    }
+}
