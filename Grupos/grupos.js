de129988d5a311a7aee14e26b0d454e42e8597c8
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2020-07-09 23:30:50 -0300

    ok

---
+++ Grupos/grupos.js
@@ -0,0 +1,9 @@
+const texto1 = 'O Jośe Simão é muito engraçado... hehehehehe'
+
+console.log(texto1.match(/(he)+/g));
+
+const texto2 = 'http://www.site.info www.escola.ninja.br google.com.ag'
+
+console.log(texto2.match(/(http:\/\/)?(www\.)?\w+.\w{2,}(\.\w{2})?/g));
+
+
