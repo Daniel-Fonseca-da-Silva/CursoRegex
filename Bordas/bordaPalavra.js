8810dcaaaa34a1d415883a5af0cd0f4245929a88
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2020-07-13 02:54:00 -0300

    Adicionado o arquivo borda palavra na pasta bordas

---
+++ Bordas/bordaPalavra.js
@@ -0,0 +1,14 @@
+const texto1 = 'dia diatonico diafragma media wikipedia bom_dia melodia radial'
+
+console.log(texto1.match(/\bdia\w+/gi));
+
+console.log(texto1.match(/\w+dia\b/gi));
+console.log(texto1.match(/\w+dia\w+/gi));
+console.log(texto1.match(/\bdia\b/gi));
+
+// Borda é não \w, que é [^A-Za-z0-9]... temos problemas com acentos!
+const texto2 = 'dia diatônico diafragma, média, wikipédia, bom-dia, melodia radial'
+
+console.log(texto2.match(/\bdia\b/gi));
+console.log(texto2.match(/(\S*)?dia(\S*)?/gi)); // a virgula nesse caso entra!
+console.log(texto2.match(/([\wÀ-ú-]*)?dia([\wÀ-ú-]*)?/gi));
