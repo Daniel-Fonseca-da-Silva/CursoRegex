ce7d14359946b4fea9702fc80460d37d433bff87
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2020-07-13 22:14:48 -0300

    ok

---
+++ receitas/files.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+const read = nomeArquivo => {
+    fs.readFileSync(`${__dirname}/originais${nomeArquivo}`, {encondig: 'utf8'})
+}
+
+const write = (nomeArquivo, conteudo) => {
+    const dirname = `${__dirname}/alterados`
+    if(!fs.existsSync(dirname))
+    {
+        fs.mkdirSync(dirname)
+    }
+
+    fs.writeFileSync(`${dirname}/${nomeArquivo}`, conteudo, { encoding: 'utf8'})
+}
+
+module.exports = {read, write}
